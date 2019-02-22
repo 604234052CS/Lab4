bb6fda89b6d7fc7a2873e76b08de57ed2c8180e2
604234052CS <604234052@parichat.skru.ac.th>
2019-02-22 16:57:01 +0700

    lab4

--- src/pages/book-detail/book-detail.ts
+++ src/pages/book-detail/book-detail.ts
@@ -30,7 +30,7 @@ ionViewWillEnter(){
     console.log('ionViewWillEnter');
     this.bookId=this.navParams.get("bookId");
     console.log(this.bookId);
-    this.bookRest.getBookList().subscribe( data =>{
+    this.bookRest. getbooklist().subscribe( data =>{
       this.book=data.filter(book=> book.bookid === this.bookId)[0];
     }
     );
